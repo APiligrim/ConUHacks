c3e91072bd10aa09ec8104a628ecd9d6783422be
Anastasiya Uraleva <anastasiyauraleva@Anastasiyas-MBP.fios-router.home>
2022-01-30 02:28:28 -0500

    added a add item page

--- Furnishare/components/navigation.component.js
+++ Furnishare/components/navigation.component.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { CaptureComponent } from "./capture.component";
 import { HomeComponent } from "./home.component";
+import { AddItemComponent } from "./add-item.component";
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -13,7 +14,9 @@ const HomeNavigator = () => (
       component={HomeComponent}
       options={{ headerShown: false }}
     /> */}
-    <Screen name="Capture" component={CaptureComponent} />
+    <Screen name="Capture" options={{ headerShown: false }} component={CaptureComponent} />
+    <Screen name="Add item"  options={{ headerShown: false }} component={AddItemComponent} />
+
   </Navigator>
 );
 
