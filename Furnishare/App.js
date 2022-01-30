560fce7174a72d87611f91cfef99885075daf621
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2022-01-29 20:04:20 -0500

    fix import

--- Furnishare/App.js
+++ Furnishare/App.js
@@ -4,12 +4,11 @@ import * as eva from "@eva-design/eva";
 import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
 import { EvaIconsPack } from "@ui-kitten/eva-icons";
 import { AppNavigator } from "./components/navigation.component";
-import { default as theme } from "./theme.json";
 
 export default () => (
   <>
     <IconRegistry icons={EvaIconsPack} />
-    <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
+    <ApplicationProvider {...eva} theme={{ ...eva.light }}>
       <SafeAreaView
         style={{ flex: 1, marginTop: StatusBar.currentHeight || 0 }}
       >
