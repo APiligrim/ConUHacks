8438d71ff7b1cb6329201a02be80dafae5a21519
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2022-01-29 21:45:29 -0500

    snap to user location

--- Furnishare/components/home.component.js
+++ Furnishare/components/home.component.js
@@ -1,5 +1,5 @@
-import React from "react";
-import MapView, { Marker } from "react-native-maps";
+import React, { useState, useEffect } from "react";
+import MapView, { Marker, Overlay } from "react-native-maps";
 import {
   Divider,
   Layout,
@@ -7,13 +7,33 @@ import {
   TopNavigation,
   Button,
   Card,
+  Icon,
 } from "@ui-kitten/components";
-import { Dimensions, View, Image } from "react-native";
+import { Dimensions, View, Image, Platform, StyleSheet } from "react-native";
+import * as Location from "expo-location";
+
 import FurnitureIcon from "../assets/chair.png";
 import ElectronicsIcon from "../assets/laptop.png";
 import ClothingIcon from "../assets/shirt.png";
 
+const PlusIcon = (props) => <Icon {...props} name="plus-circle-outline" />;
+
 export const HomeComponent = ({ navigation }) => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
   return (
     <Layout
       style={{
@@ -24,6 +44,11 @@ export const HomeComponent = ({ navigation }) => {
       }}
     >
       <MapView
+        region={{
+          ...location?.coords,
+          latitudeDelta: 0.07,
+          longitudeDelta: 0.05,
+        }}
         style={{
           width: Dimensions.get("window").width,
           height: Dimensions.get("window").height + 50,
@@ -31,6 +56,16 @@ export const HomeComponent = ({ navigation }) => {
       >
         <MapIcons />
       </MapView>
+      <Button
+        style={{
+          position: "absolute",
+          bottom: 50,
+          borderRadius: 15,
+        }}
+        accessoryLeft={PlusIcon}
+      >
+        ADD ITEM
+      </Button>
     </Layout>
   );
 };
